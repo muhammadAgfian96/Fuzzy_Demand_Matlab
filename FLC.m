1d6d03c70b8180b556e475614021fdf015f1c829
muhammadAgfian96 <muhammadagfian96@gmail.com>
2019-05-07 00:49:58 +0700

    first

---
+++ FLC.m
@@ -0,0 +1,41 @@
+a=newfis('tipper');
+a=addvar(a,'input','demand',[0 320]);
+a=addmf(a,'input',1,'Low','trapmf', [-115.2 -12.8 82.42 160]);
+a=addmf(a,'input',1,'Medium','trimf', [82.42 160 237.6]);
+a=addmf(a,'input',1,'High','trapmf', [160 237.6 332.8 435.2]);
+% figure,
+% plotmf(a,'input',1)
+
+a=addvar(a,'input','supply',[0 1307]);
+a=addmf(a,'input',2,'Low','trapmf',  [-468 -52 326.8 653.5]);
+a=addmf(a,'input',2,'Medium','trimf',  [326.8 653.5 980.3]);
+a=addmf(a,'input',2,'High','trapmf',  [653.5 980.3 1352 1768]);
+% figure,
+% plotmf(a,'input',2)
+
+
+a=addvar(a,'output','FOQ',[0 1307]);
+a=addmf(a,'output',1,'Low','trapmf',  [-468 -52 326.8 653.5]);
+a=addmf(a,'output',1,'Medium','trimf',  [326.8 653.5 980.3]);
+a=addmf(a,'output',1,'High','trapmf',  [653.5 980.3 1352 1768]);
+% figure,
+% plotmf(a,'output',1)
+
+a=addvar(a,'output','ROP',[0 680]);
+a=addmf(a,'output',2,'Very Low','trimf', [-1 0 200]);
+a=addmf(a,'output',2,'Low','trimf', [0 200 340]);
+a=addmf(a,'output',2,'Medium','trimf', [200 340 480]);
+a=addmf(a,'output',2,'High','trimf', [340 480 680]);
+a=addmf(a,'output',2,'Very High','trimf', [480 680 700]);
+% figure,
+% plotmf(a,'output',2)
+
+ruleList=[ ...
+1 1 1 1 2
+2 0 2 1 1];
+
+a=addrule(a, ruleList );
+
+showfis(a);
+showrule(a);
+% output = evalfis([ 1 2], a); % Output calculation, input = [ 1 2]
